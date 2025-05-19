bb795a8597e172ce45bdba4f5f9855b93879c428
katherinehambley <superdesigngirl@outlook.com>
2025-05-19 16:35:25 -0500

    Initial commit

---
+++ MyConverterApp2/MyConverterApp2/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MyConverterApp2;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MyConverterApp2/MyConverterApp2/MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MyConverterApp2;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("FontAwesome6Solid.otf", "FontAwesomeSolid");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
