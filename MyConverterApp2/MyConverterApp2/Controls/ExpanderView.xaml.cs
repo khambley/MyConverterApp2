b03bb0a0275636cda7ab9ecb312511e8c4fd722e
katherinehambley <superdesigngirl@outlook.com>
2025-05-21 08:24:13 -0500

    created reusable ExpanderView

---
+++ MyConverterApp2/MyConverterApp2/Controls/ExpanderView.xaml.cs
@@ -0,0 +1,48 @@
+namespace MyConverterApp2.Controls;
+
+public partial class ExpanderView : ContentView
+{
+	public static readonly BindableProperty HeaderTextProperty =
+		BindableProperty.Create(nameof(HeaderText), typeof(string), typeof(ExpanderView), string.Empty);
+
+	public static readonly BindableProperty HeaderBackgroundColorProperty =
+		BindableProperty.Create(nameof(HeaderBackgroundColor), typeof(Color), typeof(ExpanderView), Colors.LightGray);
+
+	public static readonly BindableProperty ExpanderContentProperty =
+		BindableProperty.Create(nameof(ExpanderContent), typeof(View), typeof(ExpanderView), propertyChanged: OnContentChanged);
+
+	public string HeaderText
+	{
+		get => (string)GetValue(HeaderTextProperty);
+		set => SetValue(HeaderTextProperty, value);
+	}
+
+	public Color HeaderBackgroundColor
+	{
+		get => (Color)GetValue(HeaderBackgroundColorProperty);
+		set => SetValue(HeaderBackgroundColorProperty, value);
+	}
+
+	public View ExpanderContent
+	{
+		get => (View)GetValue(ExpanderContentProperty);
+		set => SetValue(ExpanderContentProperty, value);
+	}
+	public ExpanderView()
+	{
+		InitializeComponent();
+	}
+	private static void OnContentChanged(BindableObject bindable, object oldValue, object newValue)
+	{
+		if (bindable is ExpanderView view && newValue is View newContent)
+		{
+			view.ExpanderContentContainer.Children.Clear();
+			view.ExpanderContentContainer.Children.Add(newContent);
+		}
+	}
+	private void OnHeaderTapped(object sender, EventArgs e)
+    {
+        UpArrow.IsVisible = !UpArrow.IsVisible;
+        DownArrow.IsVisible = !DownArrow.IsVisible;
+    }
+}
