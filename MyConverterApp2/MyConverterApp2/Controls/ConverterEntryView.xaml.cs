6772bae2365b418c4b71a7d1a2912f7550ed743d
katherinehambley <superdesigngirl@outlook.com>
2025-06-14 16:07:45 -0500

    changed name ConverterEntryView control

---
+++ MyConverterApp2/MyConverterApp2/Controls/ConverterEntryView.xaml.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+
+namespace MyConverterApp2.Controls;
+
+public partial class ConverterEntryView : ContentView
+{
+	public ConverterEntryView()
+	{
+		InitializeComponent();
+	}
+	public static readonly BindableProperty UnitNameProperty =
+    	BindableProperty.Create(nameof(UnitName), typeof(string), typeof(ConverterEntryView), string.Empty);
+
+	public string UnitName
+	{
+		get => (string)GetValue(UnitNameProperty);
+		set => SetValue(UnitNameProperty, value);
+	}
+	// Unit Names (ItemsSource)
+    public static readonly BindableProperty UnitNamesProperty =
+        BindableProperty.Create(nameof(UnitNames), typeof(IEnumerable), typeof(ConverterEntryView));
+
+    public IEnumerable UnitNames
+    {
+        get => (IEnumerable)GetValue(UnitNamesProperty);
+        set => SetValue(UnitNamesProperty, value);
+    }
+
+    // Selected From Unit
+    public static readonly BindableProperty SelectedFromUnitProperty =
+        BindableProperty.Create(nameof(SelectedFromUnit), typeof(string), typeof(ConverterEntryView), default(string), BindingMode.TwoWay);
+
+    public string SelectedFromUnit
+    {
+        get => (string)GetValue(SelectedFromUnitProperty);
+        set => SetValue(SelectedFromUnitProperty, value);
+    }
+
+    // Selected To Unit
+    public static readonly BindableProperty SelectedToUnitProperty =
+        BindableProperty.Create(nameof(SelectedToUnit), typeof(string), typeof(ConverterEntryView), default(string), BindingMode.TwoWay);
+
+    public string SelectedToUnit
+    {
+        get => (string)GetValue(SelectedToUnitProperty);
+        set => SetValue(SelectedToUnitProperty, value);
+    }
+
+    // Unit Value Input
+    public static readonly BindableProperty UnitValueProperty =
+        BindableProperty.Create(nameof(UnitValue), typeof(double), typeof(ConverterEntryView), default(double), BindingMode.TwoWay);
+
+    public double UnitValue
+    {
+        get => (double)GetValue(UnitValueProperty);
+        set => SetValue(UnitValueProperty, value);
+    }
+}
