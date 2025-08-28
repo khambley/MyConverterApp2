d3b7fd53a6688a86720130c1bc85823578a4ced8
katherinehambley <superdesigngirl@outlook.com>
2025-08-27 21:13:39 -0500

    add Settings.Template file

---
+++ MyConverterApp2/MyConverterApp2/Settings.Template.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyConverterApp2
+{
+    public class Settings
+    {
+        public static string CurrencyApiKey => "<API_KEY_HERE>";
+        public static string SyncfusionLicenseKey => "<License_Key_Here>";
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MyConverter App
 A .NET MAUI app that converts units of measure targeting iOS and Android.
+
+## Developers:  
+Copy keys from local Settings file to Settings.Template file, remove them before commiting to sc.
